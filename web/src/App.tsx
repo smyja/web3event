a451a2b8ee10e90eecb5244c60d3aa43ebdf340a
Smyja <akposlive59@gmail.com>
2024-09-21 06:35:13 +0100

    update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { MantineProvider } from '@mantine/core';
 import { LogTerminal } from './components/LogTerminal';
 import { theme } from './theme';
 import '@mantine/core/styles.css';
+
 export default function App() {
   return (
     <MantineProvider   theme={theme}>
